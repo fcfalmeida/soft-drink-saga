42632c1dc3e090ed47fc39f325640cc006f69804
Francisco Almeida <franciscocfalmeida@gmail.com>
2019-11-07 23:50:35 +0000

    add DialogDatabase class

---
+++ gui/dialogue/DialogueDatabase.gd
@@ -0,0 +1,22 @@
+class_name DialogueDatabase
+
+var dialogue: Dictionary
+
+func _init():
+	print("loading dialogue...")
+	load_dialogue("res://dialogue.json")
+
+# Loads dialogue data from .json file
+func load_dialogue(file_path):
+	var file = File.new()
+	assert file.file_exists(file_path)
+	
+	file.open(file_path, file.READ)
+	var _dialogue = parse_json(file.get_as_text())
+	file.close()
+	
+	self.dialogue = _dialogue
+
+# Returns the dialogue entry with the given ID or null if it doesn't exist
+func find_by_id(id):
+	return dialogue.get(id)
