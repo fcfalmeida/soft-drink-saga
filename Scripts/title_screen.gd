4776671a6ba44b7cd6813837c7fc945442e6f70b
RicardoCQB <ricardo_cqb@hotmail.com>
2019-11-15 01:31:04 +0000

    keyboard keys now work for the menu

--- Scripts/title_screen.gd
+++ Scripts/title_screen.gd
@@ -3,6 +3,7 @@ extends Control
 var scene_path_to_load
 
 func _ready():
+	$Menu/CenterRow/Buttons/PlayButton.grab_focus()
 	for button in $Menu/CenterRow/Buttons.get_children():
 		button.connect("pressed",self,"_on_Button_pressed",[button.scene_to_load])
 
